ce082c079f7ec32dd3bec3c2a81da2c23efbd625
nakul-chawla <33320317+nakul-chawla@users.noreply.github.com>
2022-10-05 14:40:21 +0530

    adding policy to bucket

--- main.tf
+++ main.tf
@@ -12,12 +12,44 @@ provider "aws" {
   region = "us-east-1" # define region as per your account
 }
 
-resource "aws_s3_bucket" "new_bucket" {
-  bucket = "deploy-ledger-project"
+resource "aws_s3_bucket" "site" {
+  bucket = "ledger-project"
+}
 
-  object_lock_enabled = false
+resource "aws_s3_bucket_website_configuration" "site" {
+  bucket = aws_s3_bucket.site.id
 
-  tags = {
-    Environment = "Prod"
+  index_document {
+    suffix = "index.html"
   }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_acl" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  acl = "public-read"
+}
+
+resource "aws_s3_bucket_policy" "site" {
+  bucket = aws_s3_bucket.site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource = [
+          aws_s3_bucket.site.arn,
+          "${aws_s3_bucket.site.arn}/*",
+        ]
+      },
+    ]
+  })
 }
